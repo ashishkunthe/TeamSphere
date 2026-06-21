de350a6c04a57614e5344f0b837f1043f8da7fa1
ashishkunthe <ashishkunthe2003@gmail.com>
2026-06-21 12:22:35 +0530

    added protected route

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,33 @@ import { Login } from "./pages/Login";
 import { Landing } from "./pages/Landing";
 import { Dashboard } from "./pages/Dashboard";
 import { Room } from "./pages/Room";
+import { ProtectedRoute } from "./components/ProtectedRoute";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Landing />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/" element={<Landing />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/room/:roomId" element={<Room />} />
+
+        <Route
+          path="/dashboard"
+          element={
+            <ProtectedRoute>
+              <Dashboard />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/room/:roomId"
+          element={
+            <ProtectedRoute>
+              <Room />
+            </ProtectedRoute>
+          }
+        />
       </Routes>
     </BrowserRouter>
   );
